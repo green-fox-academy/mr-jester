c83252d0530c5f888e2920f69e4c1223c74da5de
Jester <ehsan.salimy@gmail.com>
2018-05-14 17:51:02 +0200

    update

---
+++ Week-01/Day-04/apend-a.ts
@@ -0,0 +1,7 @@
+// -  Create an array variable named `animals`
+//    with the following content: `["kuty", "macsk", "cic"]`
+// -  Add all elements an `"a"` at the end
+let animals: string[] = ["kuty", "macsk", "cic"];
+animals.forEach(function(element) {
+    console.log (element+'a');
+});
