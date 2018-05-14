0b0896369ac39070771d0d4f97b0439017fcc905
Jester <ehsan.salimy@gmail.com>
2018-05-14 17:37:49 +0200

    update

---
+++ Week-01/Day-04/length.ts
@@ -0,0 +1,17 @@
+// -  Create a variable named `firstList`
+//    with the following content: `[1, 2, 3]`
+// -  Create a variable named `secondList`
+//    with the following content: `[4, 5]`
+// -  Log to the console if `secondList` has more elements than `firstList`
+let firstList: number[] = [1, 2, 3];
+let secondList: number[];
+secondList = [4, 5];
+if (firstList.length < secondList.length) {
+ console.log('secondList has more elements than firstList');
+    //console.log(true);
+}
+else {
+    console.log(false);
+}
+
+//console.log(firstList.length < secondList.length)
---
+++ Week-01/Day-04/swapelement.ts
@@ -0,0 +1,4 @@
+// -  Create a variable named `abc` with the following content: `["Arthur", "Boe", "Chloe"]`
+// -  Swap the first and the third element of `abc`
+let abc: string [] = ["Arthur", "Boe", "Chloe"].reverse();
+console.log(abc);
