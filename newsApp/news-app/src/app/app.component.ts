665034511bc69fd19b7c75ba38b1dc723d2ce428
mr-jester <ehsan.salimy@gmail.com>
2018-10-03 20:08:39 +0200

    the end

--- newsApp/news-app/src/app/app.component.ts
+++ newsApp/news-app/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NewsApiService } from './news-api.service';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'news-app';
+  mArticles:any;
+  mSources:any;
+
+  constructor(private newsapi:NewsApiService){}
+
+  ngOnInit() {
+    //load articles
+   this.newsapi.initArticles().subscribe(data => this.mArticles = data['articles']);
+  //load news resources
+   this.newsapi.initSources().subscribe(data=> this.mSources = data['sources']);    
+}
+searchArticles(source){
+  this.newsapi.getArticleByID(source).subscribe(data => this.mArticles = data['articles']);
+}
 }
